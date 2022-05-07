e2ca139bcb483f1208725cc2eb8e2d19d771178c
webala <webdspam@gmail.com>
2022-05-07 07:46:40 +0300

    checkout section created

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,18 +1,44 @@
 import axios from 'axios'
-import React,{useEffect,useState} from 'react'
+import React,{useEffect,useState,useRef} from 'react'
+import './cart.css'
+import {MdAdd} from 'react-icons/md'
+import {AiOutlineMinus, AiOutlineInfoCircle} from 'react-icons/ai'
 
 function Cart() {
 
     const [cartItems, setCartItems] = useState([])
     const [cart, setCart] = useState({})
+    const codeRef = useRef()
+    const nameRef = useRef()
+    const emailRef = useRef()
 
     const getCartItems = async () => {
         const url = '/cart_items'
         axios.get(url)
         .then(res => {
-            console.log(res.data)
+            var items = res.data.items
+            var cart = res.data.order
+            setCartItems(items)
+            setCart(cart)
         })
         .catch(error => console.log(error))
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+
+      const data = {
+        name: nameRef.current.value,
+        email: emailRef.current.value,
+        confirmationCode: codeRef.current.value,
+        amount: cart.get_cart_total
+      }
+
+      const url = '/process_order'
+
+      axios.post(url, data)
+      .then(res => console.log(res.data))
+      .catch(error => console.log(error))
     }
 
     useEffect(() => {
@@ -20,7 +46,98 @@ function Cart() {
     })
   return (
     <div className='cart'>
-        
+        <section className='cart-items'>
+          <div className='actions'>
+            <h2>Cart Total: {cart.get_cart_total}</h2>
+            <button className='btn btn-primary'>Checkout</button>
+          </div>
+          <table className="table">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col"></th>
+                <th scope="col">Item</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartItems.map((item) => {
+                  return (
+                    <tr>
+                      <th scope="row">1</th>
+                      <td><img className='product-img' src={item.product.imageURL} alt='img' /></td>
+                      <td>{item.product.name}</td>
+                      <td>{item.product.price}</td>
+                      <td className='quantity'>{item.quantity}<div><MdAdd /> <AiOutlineMinus /></div></td>
+                      <td>{item.get_total}</td>
+                    </tr>
+                  )
+              })}  
+            </tbody>
+          </table>
+        </section>
+
+        <section className='checkout'>
+              <div className='shipping-information'>
+                <h1>Shipping Information</h1>
+                <form onSubmit={() => handleSubmit()}>
+                  <div>
+                    <input type='text' ref={nameRef} className='form-control' placeholder="Name" required/>
+                  </div>
+
+                  <div>
+                    <input type='email' ref={emailRef}className='form-control' placeholder="Email" required/>
+                  </div>
+
+                  <div>
+                    <input type='number' className='form-control' placeholder="Phone Number" required/>
+                  </div>
+
+                  <div>
+                    <input type='text' className='form-control' placeholder="City" />
+                  </div>
+
+                  <div>
+                    <input type='text' className='form-control' placeholder="Estate"/>
+                  </div>
+
+                  <div>
+                    <input type='text' className='form-control' placeholder="Building"/>
+                  </div>
+
+                  <div>
+                    <input type='text' className='form-control' placeholder="House"/>
+                  </div>
+
+                  <div>
+                    <input type='text' className='form-control' placeholder="House Number"/>
+                  </div>
+
+                  <div>
+                    <button type='submit' className='btn btn-success'>Process Order</button>
+                  </div>
+                </form>
+              </div>
+
+              <div className='process-order'>
+                <p className='text-sm text-info'><AiOutlineInfoCircle /> Payment via M-Pesa only</p>
+                <h2>Payment Process</h2>
+                <ol>
+                  <li>Go to M-Pesa</li>
+                  <li>Paybill: 123456</li>
+                  <li>Amount: {cart.get_cart_total}</li>
+                </ol>
+
+                <div>
+                  <input type='text' ref={codeRef} className='form-control' placeholder="CONFIRMATION CODE"/>
+                </div>
+                <div>
+                  <button className='btn btn-success mt-4'>Submit Code</button>
+                </div>
+              </div>
+        </section>
     </div>
   )
 }
